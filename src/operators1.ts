cab15b9ad996ff40606822529f0190bd2715c1fa
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2024-10-07 15:53:45 +0200

    operators1 starter

---
+++ src/operators1.ts
@@ -0,0 +1,52 @@
+import { Subject, interval, map, timer, of, from, fromEvent, count, scan } from "rxjs"
+import { pictures } from "./pictures"
+import { $ } from "./helpers/dom"
+
+const timer$ = new Subject<number>()
+
+interval(86400).pipe(map(() => 'x')).subscribe(console.log)
+timer(0, 1_000).pipe(map(() => 'x')).subscribe(console.log)
+
+// Zamień strumień z kwotami w PLN na kwoty w EUR(map) - kurs euro: const eurRate = 4.4
+// map
+const pln$ = of(100, 200, 300, 400, 500)
+const eurRate = 4.4
+
+// Wybierz ze strumienia pictures wszystkie zdjęcia których szerokość jest większa od 3000px
+// filter
+const picture$ = from(pictures)
+
+// Zwróć autora zdjęcia o id = 699
+// find, map
+
+// Stwórz sekundnik(powinien emitować cokolwiek przez10 sekund)
+// interval, map
+
+// Stwórz strumień emitujący koordynaty myszy na stronie(przy kliknięciu)
+// ev.clientX
+const mouseClick$ = fromEvent(document, 'click')
+
+const mouseMove2$ = new Subject<Event>()
+document.addEventListener('click', e => mouseMove2$.next(e))
+
+// Licznik kliknięć: emituj kolejną liczbę po kliknięciu w przycisk na stronie.
+// fromEvent, 
+const mouseClickCounter$ = fromEvent(document, 'click')
+
+// let counter = 1
+mouseClickCounter$
+  .pipe(scan((counter, ev) => counter + 1, 0))
+  .subscribe(console.log)
+
+// Stwórz strumień emitujący co 1s koordynaty myszy na stronie(nie obsługujemy przypadku gdy użytkownik przestał ruszać kursorem myszy)
+// fromEvent, sampleTime, throttleTime
+const mouseMove$ = fromEvent(document, 'mousemove')
+mouseMove$.subscribe(console.log)
+
+// SearchBox Zapisz się na zdarzenie input pola tekstowego (roboczo - jest to wyszukiwarka). 
+// Emituj wpisane wartości gdy użytkownik przerwał wpisywanie na min. 300ms i wartość nie jest pusta.
+// fromEvent, filter, debounceTime(300)
+const searchBoxValue$ = fromEvent($('#searchBox')!, 'input')
+
+// Zsumuj wszystkie wartości ze strumienia EUR
+// scan, reduce
--- src/operators2.ts
+++ src/operators2.ts
@@ -1,6 +1,7 @@
-import { delay, Observable, of } from "rxjs"
+import { delay, from, fromEvent, interval, map, Observable, of, Subject, timer } from "rxjs"
 import { User } from "./models/user.model"
 import { Order } from "./models/order.model"
+import { pictures } from "./pictures"
 
 function getUser(): Observable<User> {
   return of({ name: 'John', orderId: 1 }).pipe(delay(1_000))
@@ -8,3 +9,4 @@ function getUser(): Observable<User> {
 function getOrder(id: number): Observable<Order> {
   return of({ id, products: ['Apple', 'Banana'] }).pipe(delay(2_000))
 }
+
