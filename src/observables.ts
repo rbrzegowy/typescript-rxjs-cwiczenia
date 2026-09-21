8d4c5d14a8cf05bdf1e1a2ee7e436b97d9fcaf20
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2026-09-21 12:11:00 +0200

    fix: correct spacing in comments for better readability

--- src/observables.ts
+++ src/observables.ts
@@ -3,13 +3,13 @@ import { pictures } from "./pictures"
 
 // Zadeklaruj strumień:
 
-// który emituje typ number[].Jeśli przed subskrypcją nie było emisji, powinien zwrócić pustą tablicę. 
+// który emituje typ number[]. Jeśli przed subskrypcją nie było emisji, powinien zwrócić pustą tablicę. 
 const numbers$ = null
 
-// który wyemituje typ string.Subskrybenci dostają tylko wartości emitowane po zapisie.
+// który wyemituje typ string. Subskrybenci dostają tylko wartości emitowane po zapisie.
 const string$ = null
 
-// który emituje typ number.Przy subskrypcji powinien zwrócić trzy ostatnie wyemitowane wartości (jeśli emisja już nastąpiła)
+// który emituje typ number. Przy subskrypcji powinien zwrócić trzy ostatnie wyemitowane wartości (jeśli emisja już nastąpiła)
 const number$ = null
 
 // który wyemituje po zapisie wszystkie obiekty z tablicy pictures po kolei (import z pliku pictures.ts)
